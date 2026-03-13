b0c65062a1b0c401b50fa7a61bae6b5fc6a5eede
pramodya2004 <pramodya2004dewmini@gmail.com>
2026-03-14 00:37:15 +0530

    massage

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ app.use(express.urlencoded({ extended: true }));
 
 let username = "";
 
-// GET - first page
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// POST - get name
+
 app.post("/submit", (req, res) => {
   username = req.body.name;
   res.redirect("/hello");
 });
 
-// GET - show name
+
 app.get("/hello", (req, res) => {
   res.send(`<h1>Hello, ${username}!</h1><a href="/">Go Back</a>`);
 });
